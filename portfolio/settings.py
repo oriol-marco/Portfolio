22e578b6d870a5668414220a5345f937e2febbf4
Oriol Marco <oriolmarcosan@gmail.com>
2022-07-06 10:41:28 +0200

    Heroku deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -3,6 +3,12 @@ from pathlib import Path
 import os
 from django.urls import clear_script_prefix
 
+#new
+import django_heroku
+import dj_database_url
+from decouple import config
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
@@ -121,7 +128,13 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+#new
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#new
+django_heroku.settings(locals())
