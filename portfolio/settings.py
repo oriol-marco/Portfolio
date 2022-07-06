997247f58040c2413e62b8edeb13b9d5aefa103f
Oriol Marco <oriolmarcosan@gmail.com>
2022-07-06 16:38:57 +0200

    test staticfiles

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -122,7 +122,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
